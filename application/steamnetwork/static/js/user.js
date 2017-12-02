d81dafe6c3af15f412131befd534b2d34596500e
thegazou <thegazou@romandie.com>
2017-12-02 16:59:27 +0100

    update user page add filter not fonctional

--- application/steamnetwork/static/js/user.js
+++ application/steamnetwork/static/js/user.js
@@ -1,97 +1,130 @@
 'use strict';
 
 var app = new Vue({
-  el: "#user",
-  delimiters: ["[[", "]]"],
-  data: {
-    title: "Loading",
-    page_title: "Loading",
-    page_subtitle: "",
-    laoding: true,
-    rawdata: undefined,
-    personaname: undefined,
-    state: "loading",
-  },
-  methods:
-  {
-      buttonIdClicked: function (event)
-      {
-          app.state = "Loading";
-
-          alert(app.user_id);
-      }
-  }
+    el: "#user",
+    delimiters: ["[[", "]]"],
+    data: {
+        rawdata: undefined,
+        personaname: undefined,
+        state: "loading",
+        gameidToGameMap: undefined,
+        dataPoints: undefined,
+        chart: undefined,
+        maxGame: 0,
+        isPlaytimeRangeTwoWeeks: "true",
+        data: undefined,
+    },
+    methods: {
+        updateView: function() {
+          if(app.isPlaytimeRangeTwoWeeks)
+          {
+            app.chart.options.data[0].dataPoints = app.data.get("playtime_2_weeks");
+          }
+          else {
+            app.chart.options.data[0].dataPoints = app.data.get("playtime_total");
+          }
+          app.chart.render();
+        }
+    }
 });
 
 window.onload = getUserInfos;
 
-function getUserInfos()
-{
-  fetch('/api/allinfos/' + name, {
-      method: "GET",
-    })
-  .then((resp) => resp.json())
-  .then(function(data) {
-    if (data.status === "ok") {
-      app.rawdata = data;
-      app.personaname = app.rawdata.user.persona_name;
-      app.state = "loaded";
-      setTitle(app.personaname);
-      setPage_title("Profile of " + app.personaname);
-      setPage_subtitle("Let's find some friends, shall we?");
-      setTimeout(function(){ drawBartChart(); }, 500); // without the timeout it is not loaded correctly.
+function getUserInfos() {
+    fetch('/api/allinfos/' + name, {
+            method: "GET",
+        })
+        .then((resp) => resp.json())
+        .then(function(data) {
+            if (data.status === "ok") {
+                app.rawdata = data;
+                app.personaname = app.rawdata.user.persona_name;
+                app.state = "loaded";
+                setTitle(app.personaname);
+                setPage_title("Profile of " + app.personaname);
+                setPage_subtitle("Let's find some friends, shall we?");
+                populateGameidToGameMap();
+                populateData();
+                setTimeout(function() {
+                    initBarChart();
+                }, 500); // without the timeout it is not loaded correctly.
+            }
+        }).catch((error) => console.log("Error getting the data: " + error));
+}
+
+function populateGameidToGameMap() {
+    app.gameidToGameMap = new Map();
+    app.rawdata.games.forEach(game => {
+        app.gameidToGameMap.set(game.app_id, game);
+    });
+}
+
+function populateData() {
+    var listKeys = ["playtime_2_weeks", "playtime_total", "friends_2_weeks", "friends_total"];
+    app.data = new Map();
+    var map = new Map();
+    listKeys.forEach(key => app.data.set(key, new Array()));
+    listKeys.forEach(key => map.set(key, new Map()));
+
+    app.rawdata.friends.forEach(friend => {
+        friend.games.forEach(game => {
+            if (app.gameidToGameMap.has(game.app_id)) {
+                var gameName = app.gameidToGameMap.get(game.app_id).name;
+                addIfExist(map.get("playtime_2_weeks"), gameName, game.playtime_2_weeks);
+                addIfExist(map.get("playtime_total"), gameName, game.playtime_total);
+                if (game.playtime_2_weeks > 0) {
+                    addIfExist(map.get("friends_2_weeks"), gameName, 1);
+                    addIfExist(map.get("friends_total"), gameName, 1);
+                } else if (game.playtime_total > 0) {
+                    addIfExist(map.get("friends_total"), gameName, 1);
+                }
+            }
+        });
+    });
+
+    listKeys.forEach(keyMap => {
+        map.get(keyMap).forEach((value, key, map) => app.data.get(keyMap).push({
+            "y": value,
+            "label": key
+        }));
+    });
+    listKeys.forEach(keyMap => {
+      app.data.get(keyMap).sort((a, b) => a.y - b.y);
+    });
+}
+
+function addIfExist(map, key, value) {
+    if (map.has(key)) {
+        map.set(key, map.get(key) + value);
+    } else {
+        map.set(key, value);
     }
-  }).catch((error) => console.log("Error getting the data: "+error));
 }
 
-function getMostPlayedGames(isLastTwoWeeks)
-{
-  //TODO
-}
+function initBarChart() {
+  console.log("asd+");
+    app.chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
 
-function drawBartChart()
-{
-  var data = getMostPlayedGames(true);
-  var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-
-	title:{
-		text:"Most played games by your friends during the last 2 weeks"
-	},
-	axisX:{
-		interval: 1
-	},
-	axisY2:{
-		interlacedColor: "rgba(1,77,101,.2)",
-		gridColor: "rgba(1,77,101,.1)",
-		title: "Number of players"
-	},
-	data: [{
-		type: "bar",
-		name: "games",
-		axisYType: "secondary",
-		color: "#014D65",
-		dataPoints: [
-			{ y: 3, label: "Sweden" },
-			{ y: 7, label: "Taiwan" },
-			{ y: 5, label: "Russia" },
-			{ y: 9, label: "Spain" },
-			{ y: 7, label: "Brazil" },
-			{ y: 7, label: "India" },
-			{ y: 9, label: "Italy" },
-			{ y: 8, label: "Australia" },
-			{ y: 11, label: "Canada" },
-			{ y: 15, label: "South Korea" },
-			{ y: 12, label: "Netherlands" },
-			{ y: 15, label: "Switzerland" },
-			{ y: 25, label: "Britain" },
-			{ y: 28, label: "Germany" },
-			{ y: 29, label: "France" },
-			{ y: 52, label: "Japan" },
-			{ y: 103, label: "China" },
-			{ y: 134, label: "US" }
-		]
-	}]
-});
-chart.render();
+        title: {
+            text: "Most played games by your friends during the last 2 weeks"
+        },
+        axisX: {
+            interval: 1
+        },
+        axisY2: {
+            interlacedColor: "rgba(1,77,101,.2)",
+            gridColor: "rgba(1,77,101,.1)",
+            title: "Number of players"
+        },
+        data: [{
+            type: "bar",
+            name: "games",
+            axisYType: "secondary",
+            color: "#014D65",
+            dataPoints: [],
+        }]
+    });
+    app.chart.render();
+    app.updateView();
 }
